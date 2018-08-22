477074a0b453fb09b71c1b618750e137a26d1e52
Veebhor Jain <vibhorjain13@gmail.com>
2018-08-22 22:05:11 +0530

    Corrections

--- captureFromCamera.py
+++ captureFromCamera.py
@@ -2,11 +2,13 @@
 import cv2
 import os
 
+currFileDir = os.path.dirname(__file__)  + '/' # For some reason relative paths are not working
+
 # Open the camera
 cam = cv2.VideoCapture(0)
 
 # Load HAAR Casacde 
-detector=cv2.CascadeClassifier('faceData/haarcascade_frontalface_default.xml')
+detector=cv2.CascadeClassifier(currFileDir+'faceData/haarcascade_frontalface_default.xml')
 
 # Set sample name
 i=1
@@ -21,7 +23,7 @@ while(True):
 	# Save the image to the dataset. Mark a rectangle around the face.
 	for (x,y,w,h) in faces:
 		img=gray[y:y+h,x:x+w]
-		cv2.imwrite('faceData/u6/'+str(i)+'.pgm', img)
+		cv2.imwrite(currFileDir+'faceData/u1/'+str(i)+'.pgm', img)
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
 	# Set name for next face image.
@@ -34,4 +36,3 @@ while(True):
 # Close all the active image windows.
 cam.release()
 cv2.destroyAllWindows()
-
